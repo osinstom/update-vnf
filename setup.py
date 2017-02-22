2d268fd36dcb3e5c75bf61e76b8961f0e25d1029
boucherv <valentin.boucher@orange.com>
2017-02-22 11:21:30 +0100

    initial-commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='update-workflow',
+    version='1.0',
+    author='valentin.boucher@orange.com',
+    packages=['update'],
+    install_requires=['cloudify-plugins-common==3.3'],
+)
+ 
